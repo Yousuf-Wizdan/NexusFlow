704a5113488e79077b37be8d73abeded95142541
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-10-20 13:59:42 +0530

    feat(trpc): Added BetterAuth and Auth TRPC API Enpoints

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,22 @@ import React, { Suspense } from "react";
 import { caller, getQueryClient, trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import Client from "./client";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+import { requireAuth } from "@/lib/auth-utils";
+import LogoutButton from "@/features/auth/components/LogoutButton";
 
 const Page = async () => {
-  const queryClient = getQueryClient();
 
-  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
+  await requireAuth();
+
+  const data = await caller.getUsers();
 
   return (
-    <div>
-      <HydrationBoundary state={dehydrate(queryClient)}>
-        <Suspense fallback={"Loading.."}>
-          <Client />
-        </Suspense>
-      </HydrationBoundary>
+    <div className="min-h-screen min-w-screen flex items-center justify-center">
+      Protected Page - Only visible to authenticated users.
+      {JSON.stringify(data)}
+      <LogoutButton />
     </div>
   );
 };
