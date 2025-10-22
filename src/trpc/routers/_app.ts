de269bd5ec2abe910114d6b3a6514a615d4853d9
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-10-22 14:51:17 +0530

    feat(ai): Added AI SDK and Google Gemini

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,18 @@
 import { inngest } from "@/inngest/client";
-import { createTRPCRouter, protectedProcedure } from "../init";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
+import { google } from "@ai-sdk/google";
+import { generateText } from "ai";
 
 export const appRouter = createTRPCRouter({
+  testAi: baseProcedure.mutation(async () => {
+    
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return { success: true, message: "Job Queued" };
+  }),
   getWorkflows: protectedProcedure.query(() => {
     return prisma.workflows.findMany();
   }),
@@ -10,12 +20,12 @@ export const appRouter = createTRPCRouter({
     await inngest.send({
       name: "test/hello.world",
       data: {
-        email: "user@example.com"
-      }
-    })
+        email: "user@example.com",
+      },
+    });
 
     return { success: true, message: "Job Queued" };
-  })
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
