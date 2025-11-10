7bf4bb043366434f7568490cd8fad933c75c98ff
Yousuf-Wizdan <mdyousufwizdan@gmail.com>
2025-11-11 00:11:26 +0530

    feat(nodeselector): Built and Enhanced NodeSelector Functionality

--- src/components/InitialNode.tsx
+++ src/components/InitialNode.tsx
@@ -3,18 +3,23 @@
 import { PlaceholderNode } from "@/components/reactflow/placeholder-node";
 import type { NodeProps } from "@xyflow/react";
 import { PlusIcon } from "lucide-react";
-import { memo } from "react";
+import { memo, useState } from "react";
+import { NodeSelector } from "./NodeSelectior";
 import { WorkflowNode } from "./WorkflowNode";
 
 export const InitialNode = memo((props: NodeProps) => {
+  const [selectorOpen, setSelectorOpen] = useState(false);
+
   return (
-    <WorkflowNode showToolbars={false}>
-      <PlaceholderNode {...props} onClick={() => {}}>
-        <div className="cursor-pointer flex items-center justify-center">
-          <PlusIcon className="size-4" />
-        </div>
-      </PlaceholderNode>
-    </WorkflowNode>
+    <NodeSelector open={selectorOpen} onOpenChange={setSelectorOpen}>
+      <WorkflowNode showToolbars={false}>
+        <PlaceholderNode {...props} onClick={() => setSelectorOpen(true)}>
+          <div className="cursor-pointer flex items-center justify-center">
+            <PlusIcon className="size-4" />
+          </div>
+        </PlaceholderNode>
+      </WorkflowNode>
+    </NodeSelector>
   );
 });
 
--- src/config/NodeComponents.ts
+++ src/config/NodeComponents.ts
@@ -1,10 +1,14 @@
 import { InitialNode } from "@/components/InitialNode";
+import { HttpRequestNode } from "@/features/executions/components/http-request/Node";
+import { ManualTriggerNode } from "@/features/triggers/components/manual-triggers/Node";
 import { NodeType } from "@/generated/prisma";
 import { NodeTypes } from "@xyflow/react";
 
 //Registry of Node Components
 export const nodeComponents = {
   [NodeType.INITIAL]: InitialNode,
+  [NodeType.HTTP_REQUEST]: HttpRequestNode,
+  [NodeType.MANUAL_TRIGGER]: ManualTriggerNode,
 } as const satisfies NodeTypes;
 
 export type RegesteredNodeType = keyof typeof nodeComponents;
